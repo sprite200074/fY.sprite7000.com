3b0d5d5dabedae1ede6224b4b751142b6e37d9dc
sprite200074 <h80633467@gmail.com>
2025-03-26 13:34:15 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,13 @@
+﻿{
+	"version": 1742984980,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"icon-114.png"
+	]
+}
